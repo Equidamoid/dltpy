9142599ae31ae64b23e9668339827c5830e9976e
Vladimir Shapranov <equidamoid@gmail.com>
2019-01-07 08:56:39 +0100

    dummy test stage for travis, metadata for setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 #! /usr/bin/env python3
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name='dltpy',
     version='0.1',
+    description='Pyre-python reader for DLT log files',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type="text/markdown",
+    author='Vladimir Shapranov',
+    author_email='equidamoid@gmail.com',
+    url='https://github.com/Equidamoid/dltpy',
     packages=find_packages(),
     scripts=['bin/dltpy-dump'],
     install_requires=['kaitaistruct>=0.7'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+    ]
 )
