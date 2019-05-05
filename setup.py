03bfeaccf52c7078519b915828fc9cc2d8c662ce
Vladimir Shapranov <equidamoid@gmail.com>
2019-05-05 11:23:27 +0200

    (hopefully) working setup.py for native reader, virtualenv-friendly entrypoint scripts

--- setup.py
+++ setup.py
@@ -1,6 +1,52 @@
 #! /usr/bin/env python3
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
 from pathlib import Path
+import os
+
+class CMakeExtension(Extension):
+
+    def __init__(self, name):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+
+
+class build_ext_cmake(build_ext):
+
+    def run(self):
+        for ext in self.extensions:
+            self.build_cmake(ext)
+        super().run()
+
+    def build_cmake(self, ext):
+        cwd = Path().absolute()
+
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        # example of cmake args
+        config = 'Debug' if self.debug else 'Release'
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
+            '-DCMAKE_BUILD_TYPE=' + config
+        ]
+
+        # example of build args
+        build_args = [
+            '--config', config,
+            '--', '-j4'
+        ]
+
+        os.chdir(str(build_temp))
+        self.spawn(['cmake', str(cwd)] + cmake_args)
+        if not self.dry_run:
+            self.spawn(['cmake', '--build', '.'] + build_args)
+        os.chdir(str(cwd))
+
 
 setup(
     name='dltpy',
@@ -12,7 +58,14 @@ setup(
     author_email='equidamoid@gmail.com',
     url='https://github.com/Equidamoid/dltpy',
     packages=find_packages(),
-    scripts=['bin/dltpy-dump', 'bin/dltpy-filter'],
+    ext_modules=[CMakeExtension('dltpy/native/native_dltfile')],
+    cmdclass={'build_ext': build_ext_cmake,},
+    entry_points={
+        'console_scripts': [
+            'dltpy-filter=dltpy.dltpy_filter:main',
+            'dltpy-dump=dltpy.dltpy_dump:main',
+        ],
+    },
     install_requires=['kaitaistruct>=0.7'],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
