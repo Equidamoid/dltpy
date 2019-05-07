90142e8823817a9594dfa173136ca4710ff7f4d1
Vladimir Shapranov <equidamoid@gmail.com>
2019-05-07 08:28:13 +0200

    dltpy-filter: fixed lifecycle split

--- dltpy/dltpy_filter.py
+++ dltpy/dltpy_filter.py
@@ -10,25 +10,28 @@ def main():
     prs = argparse.ArgumentParser()
     prs.add_argument('-f', '--filter', help="Messages pick, 'APP:CTX'", nargs='*')
     prs.add_argument('-s', '--lifecycle-split', action='store_true')
+    prs.add_argument('--lifecycle-threshold', type=float, default=30)
     prs.add_argument('-o', '--output')
     prs.add_argument('files', help='files to process', nargs='*')
 
     args = prs.parse_args()
     filters = [tuple(flt.split(':')) for flt in args.filter] if args.filter else None
-
     logging.warning("Filters: %s", filters)
 
     current_ts = 0
-    ts_threshold = 30
-
+    ts_threshold = args.lifecycle_threshold
     out_fn_counter = 0
     out_fn_base = Path(args.output)
     out_fd = None
+
+    if args.lifecycle_split:
+        logging.warning("LC split with threshold %.1f sec", ts_threshold) 
+
     for fn in args.files:
         dlt = df.DltFile(fn, filters, True)
         for msg in dlt:
             assert isinstance(msg, df.DltMessage)
-            if (msg.ts < current_ts < ts_threshold and args.lifecycle_split) or out_fd is None:
+            if (msg.ts - current_ts < -ts_threshold and args.lifecycle_split) or out_fd is None:
                 if not out_fd is None:
                     out_fd.close()
 
