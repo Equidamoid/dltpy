312e4f082d0b1e553551684a4e43f01be267f1a3
Vladimir Shapranov <equidamoid@gmail.com>
2019-12-24 21:06:28 +0000

    kill unused deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-(cd dltpy/native/build; make -j6)
-#cd ./dltpy/gen
-#../../opt/kaitai-struct-compiler-0.8/bin/kaitai-struct-compiler -t python *ksy
-#../../opt/kaitai-struct-compiler-0.8/bin/kaitai-struct-compiler -t cpp_stl *ksy
