cb414b94162b9082a0d6606a2a2c59364521a3f6
Vladimir Shapranov <equidamoid@gmail.com>
2020-01-21 21:04:29 +0000

    bump

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class build_ext_cmake(build_ext):
 
 setup(
     name='dltpy',
-    version='0.3.6.4',
+    version='0.3.6.5',
     description='DLT log reader',
     long_description=Path('README.md').read_text(),
     long_description_content_type="text/markdown",
